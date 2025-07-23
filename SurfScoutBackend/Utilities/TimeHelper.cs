40e5379077631222205e792351ddc923723283b5
Tim Beinert <tim.beinert@t-online.de>
2025-07-23 20:34:57 +0200

    [Added] Utility class - time helper

---
+++ SurfScoutBackend/Utilities/TimeHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace SurfScoutBackend.Utilities
+{
+    public static class TimeHelper
+    {
+        public static TimeOnly GetMeanTime(TimeOnly a, TimeOnly b)
+        {
+            int seconds_a = a.ToTimeSpan().Seconds + a.ToTimeSpan().Minutes * 60 + a.ToTimeSpan().Hours * 3600;
+            int seconds_b = b.ToTimeSpan().Seconds + b.ToTimeSpan().Minutes * 60 + b.ToTimeSpan().Hours * 3600;
+
+            int meanSeconds = (seconds_a + seconds_b) / 2;
+
+            TimeOnly meanTime = TimeOnly.FromTimeSpan(TimeSpan.FromSeconds(meanSeconds));
+
+            return meanTime;
+        }
+
+    }
+}
