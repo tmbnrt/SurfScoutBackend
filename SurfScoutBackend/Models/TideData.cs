81d0280a9c66a83a42abaf91532ece50ba365a81
Tim Beinert <tim.beinert@t-online.de>
2025-07-24 20:44:56 +0200

    [Implemented] - Stormglass :: Tide data

---
+++ SurfScoutBackend/Models/TideData.cs
@@ -0,0 +1,9 @@
+﻿namespace SurfScoutBackend.Models
+{
+    public class TideData
+    {
+        public DateTime Timestamp {  get; set; }
+        public string Type { get; set; }
+        public double HeightInMeters { get; set; }
+    }
+}
--- SurfScoutBackend/Weather/StormGlassWeatherClient.cs
+++ SurfScoutBackend/Weather/StormGlassWeatherClient.cs
@@ -65,5 +65,60 @@ namespace SurfScoutBackend.Weather
 
             return windDataList;
         }
+
+        public async Task<List<TideData>> GetTideExtremesAsync(double lat, double lng, DateOnly date)
+        {
+            var startUtc = new DateTimeOffset(date.ToDateTime(TimeOnly.MinValue)).UtcDateTime;
+            var endUtc = new DateTimeOffset(date.AddDays(1).ToDateTime(TimeOnly.MinValue)).UtcDateTime;
+
+            var isoStart = startUtc.ToString("yyyy-MM-ddTHH:mm:ssZ");
+            var isoEnd = endUtc.ToString("yyyy-MM-ddTHH:mm:ssZ");
+
+            // Call StormGlass API
+            var url = $"https://api.stormglass.io/v2/tide/point?lat={lat}&lng={lng}&start={isoStart}&end={isoEnd}";
+            var response = await _httpClient.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Stormglass API error: {response.StatusCode}");
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            // Parse JSON
+            using var doc = JsonDocument.Parse(json);
+            var root = doc.RootElement;
+            var hours = root.GetProperty("hours");
+
+            // Dynamic time zone based on coordinates
+            var timeZoneId = TimeZoneLookup.GetTimeZone(lat, lng).Result;
+            var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+
+            var tideDataList = new List<TideData>();
+
+            foreach (var hour in hours.EnumerateArray())
+            {
+                if (hour.TryGetProperty("type", out var typeProp) &&
+                    hour.TryGetProperty("height", out var heightProp) &&
+                    hour.TryGetProperty("timestamp", out var timeProp))
+                {
+                    var type = typeProp.GetString();
+                    if (type == "high" || type == "low")
+                    {
+                        var utcTime = DateTimeOffset.FromUnixTimeSeconds(timeProp.GetInt64()).UtcDateTime;
+                        var localTime = TimeZoneInfo.ConvertTimeFromUtc(utcTime, timeZoneInfo);
+
+                        tideDataList.Add(new TideData
+                        {
+                            Timestamp = localTime,
+                            Type = type,
+                            HeightInMeters = heightProp.GetDouble()
+                        });
+                    }
+                }
+            }
+
+            return tideDataList;
+        }
     }
 }
